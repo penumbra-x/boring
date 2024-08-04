e81306d2d88a1e447c8865525014e999924a6e75
0x676e67 <gngppz@gmail.com>
2024-08-04 19:52:49 +0800

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,7 +17,7 @@ if [[ "$(git diff --stat)" != '' ]]; then
 fi
 
 echo "===  Publishing boring-sys... ==="
-(cd boring-sys && cargo publish)
+(cd boring-sys && cargo publish --no-verify)
 sleep 20
 
 echo "===  Publishing boring... ==="
